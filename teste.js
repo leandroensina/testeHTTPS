584197fc529f520a1450c795976bbfc8b03ecdc1
leandroensina <leandro.ensina95@gmail.com>
2019-04-20 17:36:20 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,3 +1,6 @@
+
+document.querySelector("#acc").textContent = "YES";
+document.querySelector("#acc").className = "yes";
 window.addEventListener("devicemotion", accelerometerUpdate, true);
 
 function accelerometerUpdate(event) {
@@ -5,9 +8,15 @@ function accelerometerUpdate(event) {
    var aY = event.accelerationIncludingGravity.y*10;
    var aZ = event.accelerationIncludingGravity.z*10;
 
-   document.querySelector("#x").value = aX;
-   document.querySelector("#y").value = aY;
-   document.querySelector("#z").value = aZ;
+	document.querySelector("#x").value = aX;
+	document.querySelector("#y").value = aY;
+	document.querySelector("#z").value = aZ;
+
+	// ix aY is negative, switch rotation
+	if (aY <0) {
+		aX = -aX - 180;
+	}
+	document.querySelector("#block").style.transform="rotate("+aX+"deg)";
 
 }
 
