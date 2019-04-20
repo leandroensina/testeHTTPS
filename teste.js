938e2125393b06b28eb47998f13c55ecb8a25289
leandroensina <leandro.ensina95@gmail.com>
2019-04-20 17:13:55 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -4,5 +4,5 @@ accelerometer.addEventListener('reading', e =>
 	document.querySelector("#x").value = accelerometer.x;
 	document.querySelector("#y").value = accelerometer.y;
 	document.querySelector("#z").value = accelerometer.z;
-});
+);
 accelerometer.start();
