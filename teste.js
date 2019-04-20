6702340db0672040b91e4660e29ef7d613a9920b
leandroensina <leandro.ensina95@gmail.com>
2019-04-20 17:34:36 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,3 +1,18 @@
+window.addEventListener("devicemotion", accelerometerUpdate, true);
+
+function accelerometerUpdate(event) {
+   var aX = event.accelerationIncludingGravity.x*10;
+   var aY = event.accelerationIncludingGravity.y*10;
+   var aZ = event.accelerationIncludingGravity.z*10;
+
+   document.querySelector("#x").value = aX;
+   document.querySelector("#y").value = aY;
+   document.querySelector("#z").value = aZ;
+
+}
+
+
+/*
 let option = null;
 let sensorConstructor = null;
 
@@ -20,3 +35,4 @@ sensor.onreading = () => {
 	document.querySelector("#z").value = sensor.z;
 	
 }
+*/
