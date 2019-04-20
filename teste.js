3970204b516528be5c89a9e499489bc3cffb94bd
leandroensina <leandro.ensina95@gmail.com>
2019-04-20 17:37:55 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -1,6 +1,16 @@
 
+if (window.DeviceMotionEvent == undefined) {
+	//No accelerometer is present. Use buttons. 
+	document.querySelector("#acc").textContent = "NO";
+	document.querySelector("#acc").className = "no";
 
-window.addEventListener("devicemotion", accelerometerUpdate, true);
+}
+else {
+	document.querySelector("#acc").textContent = "YES";
+	document.querySelector("#acc").className = "yes";
+	window.addEventListener("devicemotion", accelerometerUpdate, true);
+}
+
 
 function accelerometerUpdate(event) {
    var aX = event.accelerationIncludingGravity.x*10;
@@ -10,8 +20,14 @@ function accelerometerUpdate(event) {
 	document.querySelector("#x").value = aX;
 	document.querySelector("#y").value = aY;
 	document.querySelector("#z").value = aZ;
-}
 
+	// ix aY is negative, switch rotation
+	if (aY <0) {
+		aX = -aX - 180;
+	}
+	document.querySelector("#block").style.transform="rotate("+aX+"deg)";
+
+}
 
 /*
 let option = null;
